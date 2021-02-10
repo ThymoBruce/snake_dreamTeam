3abc0727f64d988f8f3d2be76c122b66c934bea8
Thymo <Thymo@DESKTOP-RMRM8FM>
2021-02-10 18:25:05 +0100

    first commit

---
+++ snake_dreamTeam/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace snake_dreamTeam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int snakesize = 3;
+            Label[] snake = new Label[snakesize];
+            var snakeVar = new snake_remake(snake, snakesize, 15, this, Color.Red);
+        }
+    }
+}
---
+++ snake_dreamTeam/snake-remake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+namespace snake_dreamTeam
+{
+    class snake_remake
+    {
+        /* Properties */
+        Label[] snake; //the body
+        int snakeBodySize = 3; // amount of labels in 1 snake
+        int bodyMeasures = 10; //the size of the labels of a snake
+        
+        /// <summary>
+        /// create the snake
+        /// </summary>
+        public snake_remake(Label[] snake, int snakeBodySize, int snakeBodyWidthHeight, Control ctrl, Color headcolor)
+        {
+            //create the head
+            snake[0] = new Label();
+            snake[0].Size = new Size(snakeBodyWidthHeight, snakeBodyWidthHeight);
+            snake[0].BackColor = headcolor;
+            snake[0].Location = new Point(200, 100);
+            ctrl.Controls.Add(snake[0]);
+            for (int i = 1; i < snake.Length; i++)
+            {
+                snake[i] = new Label();
+                snake[i].Size = snake[0].Size;
+                snake[i].BackColor = Color.Black;
+                snake[i].Location = new Point(snake[0].Location.X - i * snakeBodyWidthHeight, snake[0].Location.Y);
+                ctrl.Controls.Add(snake[i]);
+            }
+        }
+    }
+}
